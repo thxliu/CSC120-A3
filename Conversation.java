75461f3136f15c990fad0f33f61838ccc1c9416c
Tana <thliu@smith.edu>
2023-02-14 22:19:53 -0500

    commit

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,71 @@
+import java.util.Random;
+import java.util.Scanner;
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner input = new Scanner(System.in); 
+    System.out.println("How many rounds do you want to do? ");
+    int numRounds = input.nextInt();
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Hi! What's on your mind?");
+    String response = userInput.nextLine(); 
+    String[] cannedResponses = new String[] {"Wow, really?", "I see", "Hmm, interesting"};
+
+    if (response.contains("I ")){
+      // String[] splitResponse = response.split(" ", 5);
+      // String stringResponse = splitResponse.toString();
+      String newResponse = response.replace( "I ", "You ");
+      // String newResponse = String.join("You");
+      System.out.println(newResponse + "?");
+    }
+
+    else if (response.contains("I'm")){
+      // String[] splitResponse = response.split(" ", 5);
+      // String stringResponse = splitResponse.toString();
+      String newResponse = response.replace( "I'm", "You're");
+      // String newResponse = String.join("You");
+      System.out.println(newResponse + "?");
+    }
+
+    else {
+      Random random = new Random();
+      int index = random.nextInt(cannedResponses.length);
+      System.out.println(cannedResponses[index]);
+    }
+    int i = 0;
+    // while (i < numRounds) {
+    for (i = 1; i < numRounds; i++) {
+      Scanner nextInput = new Scanner(System.in);
+      String nextResponse = nextInput.nextLine();
+
+      if (nextResponse.contains("I ")){
+        // String[] splitResponse = response.split(" ", 5);
+        // String stringResponse = splitResponse.toString();
+        String newResponse = nextResponse.replace( "I ", "You ");
+        // String newResponse = String.join("You");
+        System.out.println(newResponse + "?");
+      }
+
+      else if (nextResponse.contains("I'm")){
+        // String[] splitResponse = response.split(" ", 5);
+        // String stringResponse = splitResponse.toString();
+        String newResponse = nextResponse.replace( "I'm", "You're");
+        // String newResponse = String.join("You");
+        System.out.println(newResponse + "?");
+      }
+
+      else {
+        Random random = new Random();
+        int index = random.nextInt(cannedResponses.length);
+        System.out.println(cannedResponses[index]);
+      }
+
+
+    }
+  
+  if (i == numRounds) {
+    System.out.println("Bye!");
+    }
+
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Sarah Chavez, Robbie Do
 - Which session(s) of TA / office hours did you attend?
+    Mon 2/13, Tues 2/14
 - What are your initial impressions of Java? 
+    Since there is a lot more needed to perform an action (i.e. print), it can make it easy to overlook a small step that can throw off your code. Also, there are many shared methods between Java and Python, but accessing them in Java is different so I frequently had to look up how to apply Python concepts to Java, such as setting a variable equal to the user input. Although Java still feels pretty overwhelming and new, I feel like I am starting to have a better grasp on it. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Like I stated above, a majority of methods or method concepts are also available in Java. The biggest difference is the small technicalities such as wording or including a ";" or {} afterwards that take time getting used to. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    What worked well for me was attempting each goal of the assignment individually. I first set up the input for the number of rounds and the question ("Hi! What is on your mind?") before incorporating a for loop. I then made sure the for loop ran for however many rounds inputed by the user before moving onto mirroring the words. After mirroring the words, I attempted the canned responses. Also, writing my code out in Python terms helped me better visualize the structure of the code before going back and converting it into something Java can understand. I would advise future students to not beat themselves over still feeling more comfortable working and writing in Python! Once I stopped feeling guilty for having to look up concepts in Java that I already learned in CSC 110, it made tackling the assignment less scary. 
